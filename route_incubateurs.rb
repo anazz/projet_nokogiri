5d494214ca0c92fd7a1ee4117d51aa3867e61f8b
Ana Zegheanu <ana.zegheanu@gmail.com>
2018-04-12 19:51:05 +0200

    add comments

--- route_incubateurs.rb
+++ route_incubateurs.rb
@@ -4,6 +4,6 @@ require 'nokogiri'
 def incubateurs_france
 	doc = Nokogiri::HTML(open("http://www.mon-incubateur.com/site_incubateur/incubateurs"))
 	incubator_name = doc.css("#incubators_list span a")
-	incubator_zip_code
-	incubator_link
+	# incubator_zip_code
+	# incubator_link
 end	
